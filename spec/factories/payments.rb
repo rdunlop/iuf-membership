9a056a07b167d8b9a89f164df7d7148661d2590e
Robin Dunlop <robin@dunlopweb.com>
2019-08-22 21:30:27 -0500

    Add annotations to factories

--- spec/factories/payments.rb
+++ spec/factories/payments.rb
@@ -1,5 +1,28 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: payments
+#
+#  id           :bigint           not null, primary key
+#  amount_cents :integer
+#  currency     :string
+#  received_at  :datetime
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  member_id    :bigint           not null
+#  order_id     :string
+#
+# Indexes
+#
+#  index_payments_on_member_id  (member_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (member_id => members.id)
+#
+
+
 FactoryBot.define do
   factory :payment do
     member
