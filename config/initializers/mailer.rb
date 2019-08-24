8cb4f103900f517a7696b919b78c49c77fdfc707
Robin Dunlop <robin@dunlopweb.com>
2019-08-24 14:46:21 -0500

    Fix rubocop

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 if Rails.configuration.aws_access_key_id
   creds = Aws::Credentials.new(
     Rails.configuration.aws_access_key_id,
@@ -5,9 +7,8 @@ if Rails.configuration.aws_access_key_id
   )
 
   Aws::Rails.add_action_mailer_delivery_method(:aws_sdk,
-    credentials: creds,
-    region: Rails.configuration.aws_region
-  )
+                                               credentials: creds,
+                                               region: Rails.configuration.aws_region)
 
   ActionMailer::Base.delivery_method = :aws_sdk
 end
