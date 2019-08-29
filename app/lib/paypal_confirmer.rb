c0cf45760ef5ef9efe5e6fd1906451c869c7fc78
Robin Dunlop <robin@dunlopweb.com>
2019-08-28 20:36:13 -0500

    disable rubocop for complexity on this method

--- app/lib/paypal_confirmer.rb
+++ app/lib/paypal_confirmer.rb
@@ -14,7 +14,7 @@ class PaypalConfirmer
     create_payment(paypal_order_details)
   end
 
-  def create_payment(paypal_order_details) # rubocop:disable Metrics/MethodLength
+  def create_payment(paypal_order_details) # rubocop:disable Metrics/MethodLength,Metrics/AbcSize
     return false unless paypal_order_details[:status_code] == 200
 
     result = paypal_order_details[:result]
