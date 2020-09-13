5b25dc66c662e0a37839b1b7d930c34caa790a0e
Robin Dunlop <robin@dunlopweb.com>
2020-09-12 22:43:12 -0500

    staging server DNS has changed

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,6 +3,6 @@
 set :rails_env, 'production'
 set :branch, ENV['CIRCLE_SHA1'] || ENV['REVISION'] || ENV['BRANCH_NAME'] || 'develop'
 
-server 'ec2-18-185-126-170.eu-central-1.compute.amazonaws.com', user: 'ec2-user', roles: %w[web db]
+server 'ec2-52-58-78-3.eu-central-1.compute.amazonaws.com', user: 'ec2-user', roles: %w[web db]
 
 set :nginx_server_name, 'iuf-membership-staging.unicycling-software.com'
