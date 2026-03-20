971b2fc9e558c1b611abe09ae68b2cea5c234acd
Robin Dunlop <robin@dunlopweb.com>
2026-03-20 08:45:21 -0500

    Refactor to reduce length of paypal#create

--- app/controllers/paypal_controller.rb
+++ app/controllers/paypal_controller.rb
@@ -6,15 +6,10 @@ class PaypalController < ApplicationController
   # Ignore CSRF Token on this request
   skip_before_action :verify_authenticity_token
   before_action :skip_authorization
+  before_action :check_recent_purchase
 
   # Create a transaction and capture payment
   def create
-    member = Member.find_by(id: params[:memberID])
-    if member&.recent_purchase?
-      render json: { success: false, message: 'A payment was already received for this member in the last 24 hours.' }
-      return
-    end
-
     result = PaypalConfirmer.new.capture(params[:orderID])
     if result
       PaypalConfirmer.new.create_payment(result)
@@ -22,5 +17,14 @@ class PaypalController < ApplicationController
     else
       render json: { success: false, message: 'Error processing order' }
     end
+  end
+
+  private
+
+  def check_recent_purchase
+    member = Member.find_by(id: params[:memberID])
+    return unless member&.recent_purchase?
+
+    render json: { success: false, message: 'A payment was already received for this member in the last 24 hours.' }
   end
 end
